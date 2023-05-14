ce906b45d36ea76ed5170ae7b2f2aa73037df1bf
POISpider <35352424+usa2046@users.noreply.github.com>
2023-05-14 19:11:05 +0800

    Update KLineChartPro.tsx

--- src/KLineChartPro.tsx
+++ src/KLineChartPro.tsx
@@ -54,14 +54,10 @@ export default class KLineChartPro implements ChartPro {
           period={options.period}
           periods={
             options.periods ?? [
-              { multiplier: 1, timespan: 'minute', text: '1m' },
               { multiplier: 5, timespan: 'minute', text: '5m' },
-              { multiplier: 15, timespan: 'minute', text: '15m' },
+              { multiplier: 30, timespan: 'minute', text: '30m' },
               { multiplier: 1, timespan: 'hour', text: '1H' },
-              { multiplier: 2, timespan: 'hour', text: '2H' },
-              { multiplier: 4, timespan: 'hour', text: '4H' },
               { multiplier: 1, timespan: 'day', text: 'D' },
-              { multiplier: 1, timespan: 'week', text: 'W' },
               { multiplier: 1, timespan: 'month', text: 'M' },
               { multiplier: 1, timespan: 'year', text: 'Y' }
             ]
